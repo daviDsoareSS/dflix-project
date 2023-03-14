9360b688450c4247f6a9e82b7eb46f3f076d4b4c
daviDsoareSS <david.silva425@etec.sp.gov.br>
2023-03-14 19:43:18 -0300

    footer adicionado

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import './App.css';
 
 import Navbar from './components/nav/Navbar';
 import Main from './components/main/Main';
+import Footer from './components/footer/Footer';
 
 import { Outlet, Routes, Route} from 'react-router-dom';
 
@@ -14,6 +15,7 @@ function App() {
         <Route path='/' element={<Main/> } />
       </Routes>
       <Outlet />
+      <Footer />
     </div>
   );
 }
